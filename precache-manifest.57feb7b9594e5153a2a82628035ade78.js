ff51db54333afdf78acee8f02d14026873016b4c
bhagyasri <baagisri@gmail.com>
2020-08-10 13:16:38 +0530

    Updates

---
+++ precache-manifest.57feb7b9594e5153a2a82628035ade78.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c05d13fe2ad4939354a582ad86894ec9",
+    "url": "/shopping-cart/index.html"
+  },
+  {
+    "revision": "5461b767cb4b77c5afb9",
+    "url": "/shopping-cart/static/css/main.501656e4.chunk.css"
+  },
+  {
+    "revision": "481868bd6471a5295495",
+    "url": "/shopping-cart/static/js/2.08b6d77c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/shopping-cart/static/js/2.08b6d77c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5461b767cb4b77c5afb9",
+    "url": "/shopping-cart/static/js/main.39cda50a.chunk.js"
+  },
+  {
+    "revision": "ba80342531975976b290",
+    "url": "/shopping-cart/static/js/runtime-main.6e52efc5.js"
+  },
+  {
+    "revision": "ed9aba71561143544c0269e090e21a8d",
+    "url": "/shopping-cart/static/media/Earphones.ed9aba71.jpg"
+  },
+  {
+    "revision": "bfa591ebda3370279220ca13f9105e19",
+    "url": "/shopping-cart/static/media/ac.bfa591eb.jpg"
+  },
+  {
+    "revision": "4b38404e825a73973d59523d0c983cd4",
+    "url": "/shopping-cart/static/media/hairdryer.4b38404e.jpg"
+  },
+  {
+    "revision": "68de987cde3be26c38e094c864e99dbf",
+    "url": "/shopping-cart/static/media/mixer.68de987c.png"
+  },
+  {
+    "revision": "f215d850fd968a47a46f5da7d9cd7853",
+    "url": "/shopping-cart/static/media/room-heater.f215d850.jpeg"
+  },
+  {
+    "revision": "ab7c233b103c9ab414b96fbf73ac8dc5",
+    "url": "/shopping-cart/static/media/smart-watch.ab7c233b.jpg"
+  }
+]);
